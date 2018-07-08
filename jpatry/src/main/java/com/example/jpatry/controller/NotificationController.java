97abd2514cb0a1db61c2840467af3eb3feb6344e
DESKTOP-F109P7R\loluz <loyufgxg12345@163.com>
2018-07-08 22:23:44 +0800

    transfer data successfully

--- jpatry/src/main/java/com/example/jpatry/controller/NotificationController.java
+++ jpatry/src/main/java/com/example/jpatry/controller/NotificationController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 import java.sql.Timestamp;
 import java.util.List;
@@ -16,15 +20,19 @@ public class NotificationController {
     @Autowired
     private NotificationRepository notificationRepository;
 
-    @RequestMapping("/getNotice")
-    public List<Notification> getAll(@RequestBody String groupID){
+    @CrossOrigin
+    @RequestMapping(value="/getNotice",method = RequestMethod.POST)
+    public List<Notification> getAll(@RequestBody JSONObject data){
+        String groupID=data.getString("openid");
         return notificationRepository.findByNotificationGroupID(groupID);
     }
 
-    @RequestMapping(value="/addNotice")
-    public void save(@RequestBody String openid,@RequestBody String content){
+    @CrossOrigin
+    @RequestMapping(value="/addNotice",method = RequestMethod.POST)
+    public void save(@RequestBody JSONObject data){
 
-
+        String openid=data.getString("openid");
+        String content=data.getString("content");
         Timestamp time= new Timestamp(System.currentTimeMillis());
         Notification notificationEntity= new Notification();
         notificationEntity.setNotificationContent(content);

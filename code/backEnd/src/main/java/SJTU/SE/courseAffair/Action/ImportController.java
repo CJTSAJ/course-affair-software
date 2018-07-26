16a669ae2f9e623564d8a8ee7fe02a9b6cf3535b
DESKTOP-F109P7R\loluz <loyufgxg12345@163.com>
2018-07-26 15:20:48 +0800

    成功读取文件

--- code/backEnd/src/main/java/SJTU/SE/courseAffair/Action/ImportController.java
+++ code/backEnd/src/main/java/SJTU/SE/courseAffair/Action/ImportController.java
@@ -10,13 +10,9 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import java.math.BigDecimal;
 
 @RestController
 public class ImportController {
@@ -25,7 +21,8 @@ public class ImportController {
 
     @CrossOrigin
     @RequestMapping(value="/import",method=RequestMethod.POST)
-    public void save(MultipartFile file,String opengid){
+    public void save(@RequestParam("file") MultipartFile file, @RequestParam("groupid") String opengid){
+        System.out.println("opengid:"+opengid);
         String originalFilename = file.getOriginalFilename();
         Workbook workbook = null;
         try {
@@ -42,14 +39,16 @@ public class ImportController {
             return;
         Sheet sheet = workbook.getSheetAt(0);
         int lastRowNum = sheet.getLastRowNum();
+        System.out.println("总共有"+lastRowNum+"行数据");
         //从第一行开始第一行一般是标题
         for (int j = 1; j <= lastRowNum; j++) {
             InfoEntity infoEntity = new InfoEntity();
             infoEntity.setInfoGroupId(opengid);
             Row row = sheet.getRow(j);
             if (row.getCell(0) != null) {
-                String sno = row.getCell(0).getStringCellValue();
+                String sno = String.valueOf(new BigDecimal(row.getCell(0).getNumericCellValue()));
                 infoEntity.setInfoStuId(sno);
+                System.out.println("学号:"+sno);
             }
             if (row.getCell(1) != null) {
                 String sname = row.getCell(1).getStringCellValue();
